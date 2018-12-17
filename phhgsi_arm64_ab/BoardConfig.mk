feedc7af594ef76d1289ea4d8799dd88b67f7fc0
Preet23 <preetmpaatel@gmail.com>
2018-12-17 06:55:19 +0000

    (ZS620KL) Set SELinux Permissive

--- phhgsi_arm64_ab/BoardConfig.mk
+++ phhgsi_arm64_ab/BoardConfig.mk
@@ -2,3 +2,5 @@ include build/make/target/board/generic_arm64_ab/BoardConfig.mk
 include device/phh/treble/board-base.mk
 
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2147483648
+
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
